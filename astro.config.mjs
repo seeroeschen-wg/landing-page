5a3d080b2d8200c3c9261b76d529d202515a4972
ingokuba <ingokuba@github.com>
2024-11-02 23:37:36 +0100

    working

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import { defineConfig } from "astro/config";
 
 import robotsTxt from "astro-robots-txt";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.DEV
     ? "http://localhost:4321"
     : "https://eingang-kreuzstrasse.de/",
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), robotsTxt(), icon()],
 });
